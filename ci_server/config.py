1a6ba7f916d9f6ec14463812b48e056929db6aea
Gabriel Boachie-Danquah <gabdanq@gmial.com>
2021-04-07 15:48:23 +0000

    init tests

--- ci_server/config.py
+++ ci_server/config.py
@@ -3,11 +3,20 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 
-BASE_PATH: Path = Path(__file__).resolve(strict=True).parent.parent  # strict=True makes it an absolute path
-ENV_PATH: Path = base_path /'.env'
-load_dotenv(dotenv_path=env_path)
+BASE_PATH = Path(__file__).resolve(strict=True).parent.parent  # strict=True makes it an absolute path
+ENV_PATH = BASE_PATH /'.env'
+load_dotenv(dotenv_path=ENV_PATH)
 
-class TestConfig:
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = None
+    SQLALCHEMY_TRACK_MODIFICATIONS = None
+
+
+class TestConfig(Config):
     CLIENT_ID: str = os.environ.get('CLIENT_ID')
     CLIENT_SECRET: str = os.environ.get('CLIENT_SECRET')
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:memory//'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
---
+++ tests/test_ci_server.py
@@ -0,0 +1,20 @@
+import pytest
+from ci_server import app
+
+from ci_server import __version__
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        with app.app_context():
+            yield client
+
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Zarathustra the ci service" in response.data
+
+
+def test_version():
+    assert __version__ == '0.1.0'
