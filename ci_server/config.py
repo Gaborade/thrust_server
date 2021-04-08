8f2bcca3520c92c56bf628d227340ef974895d2c
Gabriel Boachie-Danquah <gabdanq@gmial.com>
2021-04-08 12:28:45 +0000

    ci tools

--- ci_server/config.py
+++ ci_server/config.py
@@ -1,22 +1,24 @@
 import os
-from dotenv import load_dotenv
 from pathlib import Path
+from typing import Optional
 
+from dotenv import load_dotenv
 
-BASE_PATH = Path(__file__).resolve(strict=True).parent.parent  # strict=True makes it an absolute path
-ENV_PATH = BASE_PATH /'.env'
+BASE_PATH = (
+    Path(__file__).resolve(strict=True).parent.parent
+)  # strict=True makes it an absolute path
+ENV_PATH = BASE_PATH / ".env"
 load_dotenv(dotenv_path=ENV_PATH)
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = None
-    SQLALCHEMY_TRACK_MODIFICATIONS = None
+    SQLALCHEMY_DATABASE_URI: Optional[str] = None
+    SQLALCHEMY_TRACK_MODIFICATIONS: Optional[bool] = None
 
 
 class TestConfig(Config):
-    CLIENT_ID: str = os.environ.get('CLIENT_ID')
-    CLIENT_SECRET: str = os.environ.get('CLIENT_SECRET')
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:memory//'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    CLIENT_ID: Optional[str] = os.environ.get("CLIENT_ID")
+    CLIENT_SECRET: Optional[str] = os.environ.get("CLIENT_SECRET")
+    DEBUG: bool = True
+    SQLALCHEMY_DATABASE_URI: str = "sqlite:memory//"
+    SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
